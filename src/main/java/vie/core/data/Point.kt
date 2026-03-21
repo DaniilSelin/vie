f0e8df617a72f5c072f9f42270940e2d78b3bc16
Fleych <nikita17770@gmail.com>
2026-03-21 07:24:58 +0700

    Delete src directory

--- src/main/java/vie/core/data/Point.kt
+++
@@ -1,6 +0,0 @@
-package vie.core.data
-
-data class Point(
-    val x: Int,
-    val y: Int
-)
--- src/main/java/vie/core/domain/ImageFrame.kt
+++
@@ -1,9 +0,0 @@
-package vie.core.domain
-
-import vie.core.data.Point
-
-interface ImageFrame {
-    fun getBytes(p: Point): ByteArray
-    val width: Int
-    val height: Int
-}
